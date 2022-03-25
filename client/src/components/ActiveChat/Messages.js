779430572d2f822da270b7c3969dddfc8e972897
Shea Cronin <shealcronin@gmail.com>
2022-03-25 20:35:11 +0000

    Add initial ReadMarker component t appear after  last read messages

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Box } from '@material-ui/core';
 import { SenderBubble, OtherUserBubble } from '.';
 import moment from 'moment';
+import ReadMarker from './ReadMarker';
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
   return (
     <Box>
-      {messages.map((message) => {
+      {messages.map((message, i) => {
         const time = moment(message.createdAt).format('h:mm');
 
+        // Find the next message by the sender
+        const nextMessageBySender = messages.find((message) => {
+          return message.senderId === userId && messages.indexOf(message) > i;
+        })
+
+        function isThisTheLastMessageReadByRecipient() {
+          if (nextMessageBySender) {
+            return message.readByRecipient && !nextMessageBySender.readByRecipient;
+          } else {
+            return message.readByRecipient;
+          }
+        }
+
+        const readMarker = isThisTheLastMessageReadByRecipient() ? (
+          <ReadMarker otherUser={otherUser} />
+         ) : (
+           null
+         );
+
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <Fragment key={message.id}>
+            <SenderBubble text={message.text} time={time} />
+            {readMarker}
+          </Fragment>
         ) : (
           <OtherUserBubble
             key={message.id}
