21d4ad38f311eeea037629b2adffd55f46d2059b
Shea Cronin <shealcronin@gmail.com>
2022-03-28 15:15:55 +0000

    Add UI updates in Sidebar showing number of unread messages and bolding preview text when there are unread messages

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo, useMemo } from "react";
 import { Box, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -18,6 +18,22 @@ const useStyles = makeStyles((theme) => ({
     color: "#9CADC8",
     letterSpacing: -0.17,
   },
+  bubble: {
+    backgroundColor: '#3E92FF',
+    borderRadius: '10px',
+    alignSelf: 'center',
+  },
+  unreadNumber: {
+    fontSize: 11,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    letterSpacing: -0.2,
+    padding: '3px 8px',
+  },
+  unreadPreviewText: {
+    fontWeight: 'bold',
+    color: 'black',
+  },
 }));
 
 const ChatContent = ({ conversation }) => {
@@ -26,16 +42,34 @@ const ChatContent = ({ conversation }) => {
   const { otherUser } = conversation;
   const latestMessageText = conversation.id && conversation.latestMessageText;
 
+  const memoizedNumberOfUnreadMessages = useMemo(() => {
+    const unreadMessages = conversation.messages.filter((message) => 
+      message.senderId === otherUser.id 
+      && !message.readByRecipient
+    );
+
+    return unreadMessages.length;
+  }, [conversation, otherUser]);
+ 
   return (
     <Box className={classes.root}>
       <Box>
         <Typography className={classes.username}>
           {otherUser.username}
         </Typography>
-        <Typography className={classes.previewText}>
+        <Typography className={memoizedNumberOfUnreadMessages > 0 ? 
+          `${classes.previewText} ${classes.unreadPreviewText}`
+          : classes.previewText}
+        >
           {latestMessageText}
         </Typography>
       </Box>
+      {memoizedNumberOfUnreadMessages > 0 && 
+      <Box className={classes.bubble}>
+        <Typography className={classes.unreadNumber}>
+          {memoizedNumberOfUnreadMessages}
+        </Typography>
+      </Box>}
     </Box>
   );
 };
